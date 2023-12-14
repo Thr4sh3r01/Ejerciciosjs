9b7415ca5f7314a7927a0eab17d5f04a49e68994
Thr4sh3r01 <thr4sh3r01@gmail.com>
2023-12-14 01:52:18 +0100

    ejercicios javascript

---
+++ S1/3.arrays/exercise-4.js
@@ -0,0 +1,5 @@
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push ("Morty", "Summer");
+
+const ultimoAñadido = rickAndMortyCharacters[rickAndMortyCharacters.length -1]
+console.log(ultimoAñadido);
---
+++ S1/3.arrays/exercise-5.js
@@ -0,0 +1,7 @@
+const RickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+RickAndMortyCharacters.pop();
+
+let primero = RickAndMortyCharacters[0];
+let ultimo = RickAndMortyCharacters[RickAndMortyCharacters.length -1];
+console.log(primero, ultimo);
